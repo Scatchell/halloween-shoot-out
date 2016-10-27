0976567eff9305c1801b4ac0ebf415f83da400ac
Anthony <scatchell@gmail.com>
2016-10-27 13:46:31 +0100

    Matt, Anthony - Skeleton has life and can be hit and then killed

--- Assets/DamageScript.cs
+++ Assets/DamageScript.cs
@@ -3,21 +3,32 @@ using System.Collections;
 
 public class DamageScript : MonoBehaviour {
 
+	private int health = 2;
+	private int hitDamage = 1;
+
+	private Animator animator;
+
 	// Use this for initialization
 	void Start () {
-	
+		animator = gameObject.GetComponent<Animator> ();
 	}
 
 	void OnTriggerEnter(Collider collider) {
-		Debug.Log("Collided!");
-		Debug.Log (collider);
-		Debug.Log (collider.tag);
-		if (collider.gameObject.name == "Arrow") {
-			Debug.Log("Is arrow!");
-			gameObject.GetComponent<Animator>().Play("Hit");
+		if (IsAlive ()) {
+			if (collider.gameObject.name == "Arrow") {
+				Hit ();
+			}
 		}
 	}
-	
+
+	bool IsAlive() {
+		return health > 0;
+	}
+
+	void Hit() {
+		health -= hitDamage;
+		animator.Play(health == 0 ? "Death" : "Hit");
+	}	
 	// Update is called once per frame
 	void Update () {
 	
