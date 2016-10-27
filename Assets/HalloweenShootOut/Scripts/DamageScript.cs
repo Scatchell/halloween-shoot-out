a14e895a0a24dca055849865dda74a2e97a83e57
Anthony Scatchell <ascatche@thoughtworks.com>
2016-10-27 15:29:37 +0100

    Anthony - moving some files around and trying to get the skeleton to walk

--- Assets/HalloweenShootOut/Scripts/DamageScript.cs
+++ Assets/HalloweenShootOut/Scripts/DamageScript.cs
@@ -5,6 +5,7 @@ public class DamageScript : MonoBehaviour {
 
 	private int health = 2;
 	private int hitDamage = 1;
+	private float movementPauseDuration = 2f;
 
 	private Animator animator;
 
@@ -25,8 +26,22 @@ public class DamageScript : MonoBehaviour {
 		return health > 0;
 	}
 
+	private void PauseMovement ()
+	{
+		gameObject.GetComponent<Movement> ().PauseMovement ();
+	}
+
+	private void StartMovement ()
+	{
+		gameObject.GetComponent<Movement> ().StartMovement ();
+	}
+
 	void Hit() {
 		health -= hitDamage;
+		if (health > 0) {
+			PauseMovement ();
+			Invoke ("StartMovement", movementPauseDuration);
+		}
 		animator.Play(health == 0 ? "Death" : "Hit");
 	}	
 	// Update is called once per frame
