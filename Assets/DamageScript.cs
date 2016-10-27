453c111a14b27571da2f6aa5450acf78d1a8ef13
Anthony <scatchell@gmail.com>
2016-10-27 13:16:38 +0100

    Michael, Matt, Anthony - Skeleton can be struck by arrow

---
+++ Assets/DamageScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		Debug.Log("Collided!");
+		Debug.Log (collider);
+		Debug.Log (collider.tag);
+		if (collider.gameObject.name == "Arrow") {
+			Debug.Log("Is arrow!");
+			gameObject.GetComponent<Animator>().Play("Hit");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
